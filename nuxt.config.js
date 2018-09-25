ac7d87a3052bee474437462762ffc1f5dfce4b09
Christopher Mühl <christopher@padarom.io>
2018-09-25 08:19:00 +0200

    Convert to stylus

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,18 +4,18 @@ module.exports = {
     middleware: ['auth']
   },
   head: {
-    title: 'starter',
+    title: 'mana',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Mana server manager' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
   css: [
-    '~/assets/scss/main.scss'
+    '~/assets/stylus/main.styl'
   ],
   build: {
     vendor: ['axios'],
